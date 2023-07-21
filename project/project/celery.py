d33a0f562df59e86c917c7b98affcdf8490a982b
Mina Nilay Tezer <itrmlmos0066@192.168.1.94>
2023-07-21 14:53:14 +0300

    adding celery.py file

---
+++ project/project/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redismail.settings")
+
+app = Celery("redismail")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.enable_utc = False
+
+app.conf.update(timezone="Europe/Paris")
+
+app.autodiscover_tasks()
