433e02e59504042c1c2220284fde234b835353cb
mina.tezer <mina.tezer@innovance.com.tr>
2023-07-26 13:44:22 +0300

    correcting celery.py settings

--- project/project/celery.py
+++ project/project/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redismail.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-app = Celery("redismail")
+app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.conf.enable_utc = False
